96faf1a9b2e65fa4d7b45510a801515fc11fbfc9
sandrahanyh <67375539+sandrahanyh@users.noreply.github.com>
2021-12-17 19:23:24 +0200

    Requirement 4

--- source/common/texture/texture2d.hpp
+++ source/common/texture/texture2d.hpp
@@ -12,22 +12,38 @@ namespace our {
         // This constructor creates an OpenGL texture and saves its object name in the member variable "name" 
         Texture2D() {
             //TODO: Complete this function
+            //Done:Sandra Hany 
+            // glGenTextures: generate texture names
+            //1: number of texture names to be generated 
+            // name: array in which the generated texture names are stored
             glGenTextures(1, &name);
         };
 
         // This deconstructor deletes the underlying OpenGL texture
         ~Texture2D() { 
             //TODO: Complete this function
+             //Done:Sandra Hany 
+             //glDeleteTextures :delete named textures
+             //1: number of textures to be deleted.
+             //name: Specifies an array of textures to be deleted.
+            glDeleteTextures(1,&name);
         }
 
         // This method binds this texture to GL_TEXTURE_2D
         void bind() const {
             //TODO: Complete this function
+            //Done:Sandra Hany 
+            //glBindTexture: bind a named texture to a texturing target
+            // GL_TEXTURE_2D: Specifies the target of the active texture unit to which the texture is bound
+           // name: Specifies the name of a texture.
+            glBindTexture(GL_TEXTURE_2D,name);
         }
 
         // This static method ensures that no texture is bound to GL_TEXTURE_2D
         static void unbind(){
             //TODO: Complete this function
+            //Done:Sandra Hany 
+           glBindTexture(GL_TEXTURE_2D,0);
         }
 
         Texture2D(const Texture2D&) = delete;
