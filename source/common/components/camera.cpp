4c15111ff2ac9f0ee1332465fe5079f31846fdce
Millania Sameh <millaniasameh@yahoo.com>
2021-12-21 13:13:08 +0200

    requirement 7 to be completed

--- source/common/components/camera.cpp
+++ source/common/components/camera.cpp
@@ -1,16 +1,22 @@
 #include "camera.hpp"
 #include "../ecs/entity.hpp"
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp> 
+#include <glm/gtc/matrix_transform.hpp>
 
-namespace our {
+namespace our
+{
     // Reads camera parameters from the given json object
-    void CameraComponent::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void CameraComponent::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
         std::string cameraTypeStr = data.value("cameraType", "perspective");
-        if(cameraTypeStr == "orthographic"){
+        if (cameraTypeStr == "orthographic")
+        {
             cameraType = CameraType::ORTHOGRAPHIC;
-        } else {
+        }
+        else
+        {
             cameraType = CameraType::PERSPECTIVE;
         }
         near = data.value("near", 0.01f);
@@ -20,7 +26,8 @@ namespace our {
     }
 
     // Creates and returns the camera view matrix
-    glm::mat4 CameraComponent::getViewMatrix() const {
+    glm::mat4 CameraComponent::getViewMatrix() const
+    {
         auto owner = getOwner();
         auto M = owner->getLocalToWorldMatrix();
         //TODO: Complete this function
@@ -30,22 +37,54 @@ namespace our {
         // - center is any point on the line of sight. So center can be any point (0,0,z) where z < 0. For simplicity, we let center be (0,0,-1)
         // - up is the direction (0,1,0)
         // but to use glm::lookAt, we need eye, center and up in the world state.
-        // Since M (see above) transforms from the camera to thw world space, you can use M to compute:
+        // Since M (see above) transforms from the camera to the world space, you can use M to compute:
         // - the eye position which is the point (0,0,0) but after being transformed by M
         // - the center position which is the point (0,0,-1) but after being transformed by M
         // - the up direction which is the vector (0,1,0) but after being transformed by M
         // then you can use glm::lookAt
-        return glm::mat4(1.0f);
+
+        /* 
+        =========== get the eye, center, up ===========
+            eye: ana wa2f feen
+            center: where you're looking => object location
+            up: el head f ano direction => 100: x direction, 010: y direction, 001: z direction
+        */
+
+        // Millania Sameh ===> DONE
+        glm::vec3 eye = M * glm::vec4(0, 0, 0, 0);
+        glm::vec3 center = M * glm::vec4(0, 0, -1, 0);
+        glm::vec3 up = M * glm::vec4(0, 1, 0, 0);
+        glm::mat4 camera_view = glm::lookAt(eye, center, up);
+
+        return camera_view;
     }
 
     // Creates and returns the camera projection matrix
     // "viewportSize" is used to compute the aspect ratio
-    glm::mat4 CameraComponent::getProjectionMatrix(glm::ivec2 viewportSize) const {
-        //TODO: Wrtie this function
+    glm::mat4 CameraComponent::getProjectionMatrix(glm::ivec2 viewportSize) const
+    {
+        //TODO: Write this function
         // NOTE: The function glm::ortho can be used to create the orthographic projection matrix
         // It takes left, right, bottom, top. Bottom is -orthoHeight/2 and Top is orthoHeight/2.
         // Left and Right are the same but after being multiplied by the aspect ratio
         // For the perspective camera, you can use glm::perspective
-        return glm::mat4(1.0f);
+
+        // Millania Sameh ===> DONE
+        glm::mat4 projection;
+        float aspectRatio = viewportSize.x / viewportSize.y; // get the aspect ratio = x/y
+
+        if (cameraType == CameraType::PERSPECTIVE)
+            projection = glm::perspective(fovY, aspectRatio, near, far);
+        else if (cameraType == CameraType::ORTHOGRAPHIC)
+        {
+            float left = (-orthoHeight / 2) * aspectRatio;
+            float right = (orthoHeight / 2) * aspectRatio;
+            float botton = -orthoHeight / 2;
+            float top = orthoHeight / 2;
+
+            projection = glm::ortho(left, right, botton, top);
+        }
+
+        return projection;
     }
 }
--- source/common/ecs/entity.cpp
+++ source/common/ecs/entity.cpp
@@ -4,25 +4,42 @@
 
 #include <glm/gtx/euler_angles.hpp>
 
-namespace our {
+namespace our
+{
 
-    // This function returns the transformation matrix from the entity's local space to the world space
+    // target: returns the transformation matrix from the entity's local space to the world space
+
     // Remember that you can get the transformation matrix from this entity to its parent from "localTransform"
     // To get the local to world matrix, you need to combine this entities matrix with its parent's matrix and
     // its parent's parent's matrix and so on till you reach the root.
-    glm::mat4 Entity::getLocalToWorldMatrix() const {
+    glm::mat4 Entity::getLocalToWorldMatrix() const
+    {
         //TODO: Write this function
-        return glm::mat4(1.0f);
+
+        // Millania Sameh ===> DONE
+        glm::mat4 transformation_matrix = glm::mat4(1.0f); // initialize an identity matrix
+        Entity *current = parent;                          // initialize current's parent pointer
+        while (current != nullptr)                         // while loop to get all the parents up to the root
+        {
+            transformation_matrix = localTransform.toMat4() * transformation_matrix; // ===>TODO check this order
+            current = current->parent;                                               // advance the pointer to get next parent
+        }
+        return transformation_matrix;
     }
 
     // Deserializes the entity data and components from a json object
-    void Entity::deserialize(const nlohmann::json& data){
-        if(!data.is_object()) return;
+    void Entity::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
         name = data.value("name", name);
         localTransform.deserialize(data);
-        if(data.contains("components")){
-            if(const auto& components = data["components"]; components.is_array()){
-                for(auto& component: components){
+        if (data.contains("components"))
+        {
+            if (const auto &components = data["components"]; components.is_array())
+            {
+                for (auto &component : components)
+                {
                     deserializeComponent(component, this);
                 }
             }
